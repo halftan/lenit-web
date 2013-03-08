1a2c8e07964d52184330577ce69207ed87c924d8
HalFtaN <half.a.tan@gmail.com>
2013-03-08 12:00:09 +0800

    performance enhanced when user not followed anybody
    Signed-off-by: HalFtaN <half.a.tan@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,16 +52,18 @@ class UsersController < ApplicationController
   def show
     authorize! :show, current_user, :message => "You are not authorized to this page!"
     @user = User.find(params[:id])
-    if params[:filter] == "attended"
-      @events = @user.events
-    elsif params[:filter] == "followed"
+    if params[:filter] != "attended" and @user.followeds.present?
+      if params[:filter] == "followed"
       @events = @user.followeds.inject([]) do |arr, guy|
         arr + guy.get_all_events
       end
-    else
-      @events = @user.followeds.inject([]) do |arr, guy|
-        arr + guy.get_all_events
-      end + @user.events
+      else
+        @events = @user.followeds.inject([]) do |arr, guy|
+          arr + guy.get_all_events
+        end + @user.events
+      end
+    else # params[:filter] == "attended"
+      @events = @user.events
     end
   end
 
