a49e0d934ce4d74ba303a589cd79d0564c7f1efa
HalFtaN <half.a.tan@gmail.com>
2013-05-27 00:21:53 +0800

    home screen tab pill completed! Signed-off-by: HalFtaN <half.a.tan@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,18 +7,23 @@ class HomeController < ApplicationController
       @user = current_user
       @signed_in = true
 
-      # TODO: make this AJAX
-      if @user.followeds.present?
-        if params[:filter] == "attended"
-          @events = @user.events
-        else # params[:filter] == "followed"
-          @events = @user.followeds.inject([]) do |arr, fol|
-            arr + fol.get_all_events
-          end
-        end
-      else # show all events
-        @events = Event.limit(10).order("created_at DESC")
+      @attended_events = @user.events
+      @focused_events = @user.followeds.inject([]) do |arr, followed|
+        arr + fol.get_all_events
       end
+
+      # # TODO: make this AJAX
+      # if @user.followeds.present?
+      #   if params[:filter] == "attended"
+      #     @events = @user.events
+      #   else # params[:filter] == "followed"
+      #     @events = @user.followeds.inject([]) do |arr, fol|
+      #       arr + fol.get_all_events
+      #     end
+      #   end
+      # else # show all events
+      #   @events = Event.limit(10).order("created_at DESC")
+      # end
     else
       @user = User.new
       @user.email = nil
