8f5b61994cf6e4dc90bb03b59a1fd7c83368384a
HalFtaN <half.a.tan@gmail.com>
2013-05-27 16:48:54 +0800

    my_event api will return attended events Signed-off-by: HalFtaN <half.a.tan@gmail.com>

--- app/controllers/api/my_events_controller.rb
+++ app/controllers/api/my_events_controller.rb
@@ -2,17 +2,17 @@ class Api::MyEventsController < Api::ApiBaseController
   before_filter :authenticate_user!
   
   def index
-    @events = current_user.personal_events
+    @events = current_user.attended_events.where("events.id > ?", params[:latest_id] || 0)
     render 'api/events/index'
   end
 
   def show
-    @event = current_user.personal_events.find(params[:id])
+    @event = current_user.attended_events.find(params[:id])
     render 'api/events/show'
   end
 
   def update
-    @event = current_user.personal_events.find(params[:id])
+    @event = current_user.attended_events.find(params[:id])
     if @event.update_attributes(params[:event])
       render :json=> {:success=>true, :message=>I18n.t("controller.success")}, :status=>200
     else
